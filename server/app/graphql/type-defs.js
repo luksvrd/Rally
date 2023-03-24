7b075992613f1c04e89eb57bfc0ede950f9fdcd4
EllieWalsch <elliewalsch4@gmail.com>
2023-03-24 10:58:18 -0500

    Add user, habit, and group types

--- server/app/graphql/type-defs.js
+++ server/app/graphql/type-defs.js
@@ -1,3 +1,24 @@
-const typeDefs = ``;
+const typeDefs = `
+type User {
+    id: ID!
+    username: String!
+    email: String!
+    password: String!
+    habits: [Habit]
+}
+
+type Habit {
+    habitID: ID!
+    habitName: String!
+    dayStreak: INT
+    longestStreak: INT
+}
+
+type Group {
+    groupID: ID!
+    groupName: String!
+    members: [User]
+}
+`;
 
 export default typeDefs;
