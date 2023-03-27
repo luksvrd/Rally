721c642ec4e3d698430e140f84f9c530d08d5873
EllieWalsch <elliewalsch4@gmail.com>
2023-03-27 11:08:16 -0500

    Add query resolvers

--- server/app/graphql/resolvers.js
+++ server/app/graphql/resolvers.js
@@ -1,3 +1,22 @@
-const resolvers = {};
+import userController from "../user/controller.js";
+import { handleError } from "../utils.js";
+
+function handleNoUser(msg) {
+  handleError(new Error(msg), "UNAUTHENTICATED");
+}
+
+const resolvers = {
+  Query: {
+    currentUser(_, __, { user }) {
+      return user;
+    },
+    async getOneUser(_, { userId }) {
+      return await userController.getUserById(userId);
+    },
+    async getAllUsers() {
+      return await userController.index();
+    },
+  },
+};
 
 export default resolvers;
