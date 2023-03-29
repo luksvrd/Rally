fcfd6d7a8ce22d2012c45aa11731cb57786f7140
Eli Baynes <sbaynes1994@gmail.com>
2023-03-28 21:03:14 -0500

    style footer text

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-4 text-center">
+    <footer className=" fixed inset-x-1 bottom-2 mt-4 text-center text-xs text-light-grey">
       <p>© {new Date().getFullYear()} - All Rights Reserved</p>
     </footer>
   );
