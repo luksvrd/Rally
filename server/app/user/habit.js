0565332a74e3bff683a13acce65c5da2399d0e5e
masonMO25 <masonneale@gmail.com>
2023-03-28 19:56:05 -0500

    updated habit

--- server/app/user/habit.js
+++ server/app/user/habit.js
@@ -6,7 +6,6 @@ export const HabitSchema = new Schema(
     description: { type: String},
     frequency: {type: Number, default: 1},
     streak: {type: Boolean, default: false},
-    completed: {type: Schema.Types.Object.Id, ref: "Group"},
   },
   {
     versionKey: false,
