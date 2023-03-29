6c541dea2e6dfc6c89187151dfb1f6c6c89edb5f
Eli Baynes <sbaynes1994@gmail.com>
2023-03-28 21:38:45 -0500

    WIP login styling

--- client/src/pages/login-register.jsx
+++ client/src/pages/login-register.jsx
@@ -4,11 +4,11 @@ import { Input } from "../components/form";
 export default function LoginRegister() {
   const [isRegistering, setIsRegistering] = useState(false);
   return (
-    <main>
-      <h2 className="text-center">
-        {isRegistering ? "Register a New Account" : "Login Account"}
+    <main className=" mx-9 my-20 rounded-md bg-white bg-opacity-25 py-2">
+      <h2 className="py-5 text-center text-lg font-bold">
+        {isRegistering ? "Register a New Account" : "Log in to your Account"}
       </h2>
-      <form className="flex flex-col items-center gap-y-2 px-4">
+      <form className="flex flex-col items-center gap-y-3 px-4">
         <Input
           type="text"
           label="Username"
@@ -36,7 +36,7 @@ export default function LoginRegister() {
         />
         <button
           type="submit"
-          className="button mt-4 bg-green-500 hover:bg-green-300"
+          className="my-2 rounded-lg border-2 border-black bg-white px-6"
         >
           {isRegistering ? "Register" : "Login"}
         </button>
@@ -45,7 +45,7 @@ export default function LoginRegister() {
           onClick={() => {
             setIsRegistering((prev) => !prev);
           }}
-          className="text-center text-sm text-gray-500 hover:text-gray-300"
+          className="text-center text-sm font-bold text-light-grey hover:scale-110"
         >
           {isRegistering ? "Already have an account?" : "Need to register?"}
         </button>
