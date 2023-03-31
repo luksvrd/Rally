228ca9e90a11029ea5b29636a808bbf9c5dcd6f1
Eli Baynes <sbaynes1994@gmail.com>
2023-03-30 21:09:46 -0500

    add user importing to profile

--- client/src/pages/user.jsx
+++ client/src/pages/user.jsx
@@ -1,31 +1,41 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useQuery } from "@apollo/client";
+import { CURRENT_USER } from "../schema/queries";
+
 export default function User() {
-  return (
-    <div className="middle">
-      <h1 id="username" className="my-4 text-3xl">
-        User
-      </h1>
+  const { loading, error, data } = useQuery(CURRENT_USER);
 
-      <div>
-        <h2 id="habits" className=" profile-headers my-3">
-          Today's Goals
-        </h2>
-        <ul id="habit-list"></ul>
-      </div>
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  else
+    return (
+      <div className="middle">
+        <h1 id="username" className="my-4 text-3xl">
+          {data.currentUser.username}
+        </h1>
 
-      <div>
-        <h2 id="groups" className="profile-headers my-3">
-          Groups
-        </h2>
-        <ul id="group-list"></ul>
-      </div>
+        <div id="icons"></div>
 
-      <div>
-        <h2 id="friends" className="profile-headers my-3">
-          Friends
-        </h2>
-        <ul id="friend-list"></ul>
+        <div>
+          <h2 id="habits" className=" profile-headers my-3">
+            Today's Goals
+          </h2>
+          <ul id="habit-list"></ul>
+        </div>
+
+        <div>
+          <h2 id="groups" className="profile-headers my-3">
+            Groups
+          </h2>
+          <ul id="group-list"></ul>
+        </div>
+
+        <div>
+          <h2 id="friends" className="profile-headers my-3">
+            Friends
+          </h2>
+          <ul id="friend-list"></ul>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
