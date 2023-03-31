96321e0a47962f0ac6f1b3c4002412bedfccbddf
Eli Baynes <sbaynes1994@gmail.com>
2023-03-30 20:27:18 -0500

    add skeleton framework for the user's page

--- client/src/pages/user.jsx
+++ client/src/pages/user.jsx
@@ -1,6 +1,31 @@
+/* eslint-disable react/no-unescaped-entities */
 export default function User() {
-    return (
-        <h1>user</h1>
-    );
-  }
-  
+  return (
+    <div className="middle">
+      <h1 id="username" className="my-4 text-3xl">
+        User
+      </h1>
+
+      <div>
+        <h2 id="habits" className=" profile-headers my-3">
+          Today's Goals
+        </h2>
+        <ul id="habit-list"></ul>
+      </div>
+
+      <div>
+        <h2 id="groups" className="profile-headers my-3">
+          Groups
+        </h2>
+        <ul id="group-list"></ul>
+      </div>
+
+      <div>
+        <h2 id="friends" className="profile-headers my-3">
+          Friends
+        </h2>
+        <ul id="friend-list"></ul>
+      </div>
+    </div>
+  );
+}
