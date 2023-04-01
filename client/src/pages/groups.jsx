a32ccf068ea39160a54b3c113982b3547e8067c4
NhatWin <nhat0933@gmail.com>
2023-03-31 23:18:02 -0500

    Create group card

--- client/src/pages/groups.jsx
+++ client/src/pages/groups.jsx
@@ -1,4 +1,13 @@
 import CreateGroup from "../components/create-group";
+import GroupsCard from "../components/group-card";
 export default function Groups() {
-  return <CreateGroup />;
+  return (
+    <>
+      <CreateGroup />
+      <div className="flex flex-col items-center">
+        <GroupsCard />
+        <GroupsCard />
+      </div>
+    </>
+  );
 }
