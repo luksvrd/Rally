929c4a8cb711f146a4ea99da7c8381703b33310c
Eli Baynes <sbaynes1994@gmail.com>
2023-04-02 10:40:25 -0500

    temp remove group cards

--- client/src/pages/groups.jsx
+++ client/src/pages/groups.jsx
@@ -1,11 +1,8 @@
 import CreateGroup from "../components/create-group";
-import GroupsCard from "../components/group-card";
 export default function Groups() {
   return (
     <>
       <CreateGroup />
-      <div className="flex flex-col items-center">
-        <GroupsCard />
-        <GroupsCard />
-      </div>
     </>
+  );
+}
