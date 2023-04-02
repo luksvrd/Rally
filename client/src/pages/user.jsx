44b8f36c99961985739069a6962a5aa39a3c8e6d
Eli Baynes <sbaynes1994@gmail.com>
2023-04-01 21:45:29 -0500

    WIP mobile styling

--- client/src/pages/user.jsx
+++ client/src/pages/user.jsx
@@ -10,15 +10,23 @@ export default function User() {
   const groups = data?.currentUser.groups;
 
   const habitListItems = habits?.map((habit) => (
-    <li key={habit._id} className="flex rounded-md bg-white bg-opacity-25 px-4">
-      <h3>{habit.name}</h3>
-      <p>{habit.streak}</p>
-      <input type="checkbox" />
+    <li
+      key={habit._id}
+      className="w-100 items middle my-2 rounded-md bg-white bg-opacity-25 px-4 py-2"
+    >
+      <div className="flex">
+        <h3 className="text mx-2 font-semibold">{habit.name}</h3>
+        <input type="checkbox" className="border-2 border-rally-blue" />
+      </div>
+      <div className="just flex">
+        <p className="mr-1 text-xs">Current streak:</p>
+        <p className="ml-1 text-xs">{habit.streak} days</p>
+      </div>
     </li>
   ));
 
   const groupListItems = groups?.map((group) => (
-    <li key={group._id}>
+    <li key={group._id} className="rounded-md bg-white bg-opacity-25 px-4">
       <h3>{group.name}</h3>
     </li>
   ));
@@ -30,27 +38,34 @@ export default function User() {
   else
     return (
       <div className="middle">
-        <h1 id="username" className="my-4 text-3xl">
-          Welcome, {data.currentUser.username}
+        <h1 id="username" className="my-4 text-3xl font-bold drop-shadow">
+          {data.currentUser.username}
         </h1>
 
-        <div id="icons" className="grid grid-cols-3 place-items-center">
-          <img
-            src="../src/icons/reading1st.png"
-            alt="reading"
-            className="w-2/3"
-          />
-          <img src="../src/icons/walking.png" alt="running" className="w-2/3" />
-          <img
-            src="../src/icons/meditating3rd.png"
-            alt="meditation"
-            className="w-2/3"
-          />
-          <img src="../src/icons/water2nd.png" alt="water" className="w-2/3" />
+        <div className="middle">
+          <h2 className="profile-headers mb-1">Your Badges</h2>
+          <div id="icons" className="grid grid-cols-3 place-items-center">
+            <img
+              src="../src/icons/reading1st.png"
+              alt="reading"
+              className="icon"
+            />
+            <img
+              src="../src/icons/walking.png"
+              alt="running"
+              className="icon"
+            />
+            <img
+              src="../src/icons/meditating3rd.png"
+              alt="meditation"
+              className="icon"
+            />
+            <img src="../src/icons/water2nd.png" alt="water" className="icon" />
+          </div>
         </div>
 
         <div className="middle my-2 w-4/5">
-          <h2 id="habits" className=" profile-headers my-3">
+          <h2 id="habits" className=" profile-headers mb-1 mt-3">
             Today's Goals
           </h2>
           <ul>{habitListItems}</ul>
