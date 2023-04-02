27cc874f7153c602097594b3b24c4bd56f0be918
EllieWalsch <elliewalsch4@gmail.com>
2023-04-02 16:03:16 -0500

    Display group name

--- client/src/pages/leaderboard.jsx
+++ client/src/pages/leaderboard.jsx
@@ -1,20 +1,23 @@
 import { useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
 import { GET_ONE_GROUP } from "../schema/queries";
 
 export default function Leaderboard() {
-  //   const params = useParams();
-  const groupId = window.location.href.split("/")[4];
-  console.log(groupId, "window");
-  const { data } = useQuery(GET_ONE_GROUP, {
+  const params = useParams();
+  const groupId = params.groupId;
+
+  const { loading, error, data } = useQuery(GET_ONE_GROUP, {
     variables: { groupId },
   });
-  console.log(data, "data");
 
-  return (
-    <div className="middle">
-      <h2 id="groups" className="profile-headers my-3">
-        Hello
-      </h2>
-    </div>
-  );
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  else
+    return (
+      <div className="middle">
+        <h2 id="groups" className="profile-headers my-3">
+          {data.getOneGroup.name}
+        </h2>
+      </div>
+    );
 }
