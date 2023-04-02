4bef99d88fd29796f8c38c3b00b1bc80a74615c6
EllieWalsch <elliewalsch4@gmail.com>
2023-04-02 17:04:05 -0500

    WIP Find the habit that matches the group

--- client/src/pages/leaderboard.jsx
+++ client/src/pages/leaderboard.jsx
@@ -13,8 +13,20 @@ export default function Leaderboard() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
   else {
+    console.log(data, "data");
+
     const members = data?.getOneGroup.members;
-    console.log(members);
+    console.log(members, "members");
+
+    const habitArray = members.map((member) => {
+      return member.habits;
+    });
+    console.log(habitArray, "habits");
+
+    const found = habitArray.find(
+      (element) => element.name === data.getOneGroup.description
+    );
+    console.log(found, "found");
 
     const memberListItems = members?.map((member) => (
       <li key={member.username} className="semi-t-card my-2 px-12 py-2 text-xl">
