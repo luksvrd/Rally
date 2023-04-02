fbf853c2f8216452184341dedcda33e945d54b82
EllieWalsch <elliewalsch4@gmail.com>
2023-04-02 11:46:56 -0500

    Render GroupList

--- client/src/pages/groups.jsx
+++ client/src/pages/groups.jsx
@@ -1,8 +1,18 @@
+import { useQuery } from "@apollo/client";
 import CreateGroup from "../components/create-group";
+import GroupList from "../components/group-list";
+import { CURRENT_USER } from "../schema/queries";
+
 export default function Groups() {
-  return (
-    <>
-      <CreateGroup />
-    </>
-  );
+  const { loading, error } = useQuery(CURRENT_USER);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  else
+    return (
+      <>
+        <CreateGroup />
+        <GroupList />
+      </>
+    );
 }
