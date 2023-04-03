d10dd1047f23733250d1db5841d54df21e1b6c73
EllieWalsch <elliewalsch4@gmail.com>
2023-04-02 19:23:13 -0500

    WIP display user's streak

--- client/src/pages/leaderboard.jsx
+++ client/src/pages/leaderboard.jsx
@@ -25,15 +25,39 @@ export default function Leaderboard() {
 
     const description = data.getOneGroup.description;
 
-    const found = habitArray[0].find((habit) => habit.name === description);
-    console.log(found, "found");
+    // const found = habitArray[0].find((habit) => habit.name === description);
+    // console.log(found, "found");
+    const streakArray = [];
 
-    const memberListItems = members?.map((member) => (
-      <li key={member.username} className="semi-t-card my-2 px-12 py-2 text-xl">
-        <h3>{member.username}</h3>
-        <h3>{found.streak}</h3>
-      </li>
-    ));
+    for (let i = 0; i < habitArray.length; i++) {
+      const found = habitArray[i].find((habit) => habit.name === description);
+      console.log(found.streak, "found");
+      streakArray.push(found.streak);
+    }
+
+    console.log(streakArray);
+
+    // const memberListItems = members?.map((member) => (
+    //   <li key={member.username} className="semi-t-card my-2 px-12 py-2 text-xl">
+    //     <h3>{member.username}</h3>
+    //   </li>
+    // ));
+
+    const memberListItems = members?.map((member) => {
+      const habit = member.habits.find((h) => h.name === description);
+      const streak = habit?.streak || 0;
+      return (
+        <li
+          key={member.username}
+          className="semi-t-card my-2 px-12 py-2 text-xl"
+        >
+          <h3>{member.username}</h3>
+          <h3>{streak}</h3>
+        </li>
+      );
+    });
+
+    // const streakListItems = streakArray?.map((streak) => )
 
     return (
       <div className="middle">
