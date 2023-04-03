47ed5a3ab9f34025f041ae6bd7cc20048d15141f
Eli Baynes <sbaynes1994@gmail.com>
2023-04-03 16:07:15 -0500

    dynamic footer to remain at bottom of html

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,6 +1,16 @@
+import { useLocation } from "react-router-dom";
+
 export default function Footer() {
+  const location = useLocation();
+  const path = location.pathname;
+  const isUserOrGroups = path === "/user" || path === "/groups";
+
   return (
-    <footer className=" relative inset-x-1 bottom-2 mt-10 text-center text-xs text-light-grey">
+    <footer
+      className={`${
+        isUserOrGroups ? "relative" : "fixed"
+      } inset-x-1 bottom-2 mt-10 text-center text-xs text-light-grey`}
+    >
       <p>© {new Date().getFullYear()} - All Rights Reserved</p>
     </footer>
   );
