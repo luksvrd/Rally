657a0ad4b978e0528c8fd486d198315f4bbdd368
NhatWin <nhat0933@gmail.com>
2023-04-02 22:19:25 -0500

    Add logout button and dynamic navbar

--- client/src/components/navigation.jsx
+++ client/src/components/navigation.jsx
@@ -1,21 +1,36 @@
 import { Link } from "react-router-dom";
 
 export default function Navigation() {
+  const token = window.localStorage.getItem("token");
+
+  const logout = () => {
+    window.localStorage.removeItem("token");
+    window.location.href = "/";
+  };
+
   return (
     <nav className="bg-transparent">
       <ul className="flex justify-between text-light-grey">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/habit">Habits</Link>
-        </li>
-        <li>
-          <Link to="/user">Profile</Link>
-        </li>
-        <li>
-          <Link to="/groups">Groups</Link>
-        </li>
+        {token ? (
+          <>
+            <li>
+              <Link to="/habit">Habits</Link>
+            </li>
+            <li>
+              <Link to="/user">Profile</Link>
+            </li>
+            <li>
+              <Link to="/groups">Groups</Link>
+            </li>
+            <li>
+              <button onClick={logout}>Logout</button>
+            </li>
+          </>
+        ) : (
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+        )}
         <li>
           <Link to="/about">About</Link>
         </li>
