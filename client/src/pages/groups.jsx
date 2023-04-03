e01b973a5badb133864ec29a3c44588be939b977
Eli Baynes <sbaynes1994@gmail.com>
2023-04-03 15:46:21 -0500

    get updated group page copied over

--- client/src/pages/groups.jsx
+++ client/src/pages/groups.jsx
@@ -4,18 +4,15 @@ import GroupList from "../components/group-list";
 import { CURRENT_USER } from "../schema/queries";
 
 export default function Groups() {
-  const groups = useQuery(GET_GROUPS);
-  const groupData = groups?.data.getAllGroups;
-  console.log(groupData);
-  return (
-    <>
-      <CreateGroup />
-      <div className="flex flex-col items-center">
-        {groupData.map((group) => {
-          return <GroupsCard group={group} key={group.id} />;
-        })}
-      </div>
-    </>
-  );
+  const { loading, error, data } = useQuery(CURRENT_USER);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  else
+    return (
+      <>
+        <CreateGroup />
+        <GroupList currentUser={data.currentUser.id} />
+      </>
+    );
 }
