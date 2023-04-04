809ed50db0c8cf897e6ac9fecd7241808950be17
NhatWin <nhat0933@gmail.com>
2023-04-03 21:02:43 -0500

    Re-add deleted commits

--- client/src/components/group-list.jsx
+++ client/src/components/group-list.jsx
@@ -8,32 +8,36 @@ export default function GroupList({ currentUser }) {
   const [addMember] = useMutation(JOIN_GROUP);
   const userGroups = currentUser.groups;
 
-  const groups = data?.getAllGroups;
-
   console.log(currentUser, "from groupList");
 
-  const groupListItems = groups?.map((group) => (
-    <li
-      key={group._id}
-      className="semi-t-card mx-5 mb-2 flex justify-between px-2"
-    >
-      <div>
-        <h3 className="text-lg font-semibold">{group.name}</h3>
-        <h4 className="text-xs text-gray-600">{group.description}</h4>
-      </div>
-      <button
-        type="button"
-        className="my-2 rounded-lg border-2 border-black bg-white px-5 font-semibold drop-shadow-md hover:border-rally-blue"
-        onClick={() =>
-          addMember({
-            variables: { groupId: group.id, userId: currentUser },
-          })
-        }
+  const allGroupsThatUserIsNotIn = data?.getAllGroups
+    .filter(
+      (group) => !userGroups.find((userGroup) => userGroup.id === group.id)
+    )
+    .map((group) => (
+      <li
+        key={group._id}
+        className="semi-t-card mx-5 mb-2 flex justify-between px-2"
       >
-        Join
-      </button>
-    </li>
-  ));
+        <div>
+          <h3 className="text-lg font-semibold">{group.name}</h3>
+          <h4 className="text-xs text-gray-600">{group.description}</h4>
+        </div>
+        <button
+          type="button"
+          className="my-2 rounded-lg border-2 border-black bg-white px-5 font-semibold drop-shadow-md hover:border-rally-blue"
+          onClick={() =>
+            addMember({
+              variables: { groupId: group.id, userId: currentUser.id },
+            }).then(() => {
+              window.location.href = "/user";
+            })
+          }
+        >
+          Join
+        </button>
+      </li>
+    ));
 
   return (
     <div className="middle">
