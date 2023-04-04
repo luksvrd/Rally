637112dae6a1fb0073f1338d99b3d8677c531258
luksvrd <ljvy9b@gmail.com>
2023-04-03 21:28:47 -0500

    fix file pathing

--- client/src/components/form/index.js
+++ client/src/components/form/index.js
@@ -1,2 +1,2 @@
-export { default as Input } from "./Input";
-export { default as Select } from "./Select";
+export { default as Input } from "./input";
+export { default as Select } from "./select";
